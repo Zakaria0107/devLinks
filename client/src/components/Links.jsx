d3e034dd0e1ec99870b8f21e911c3fc50b1c54ba
Zakaria bijaddigune <96249501+Zakaria0107@users.noreply.github.com>
2024-01-09 14:55:51 +0100

    almost finish links and profile

--- client/src/components/Links.jsx
+++ client/src/components/Links.jsx
@@ -1,7 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoMdAdd } from "react-icons/io";
 import { LuEqual } from "react-icons/lu";
+import { FaChevronDown } from "react-icons/fa";
+
 const Links = ({listLinks , setListlink}) => {
+
+  const [openLinks, setOpenLinks] = useState({})
+  const [selectedPlatform , setSelectedPlatform] = useState('')
+  const platforms = [
+    "Github",
+    "Frontend Mentor",
+    "Twitter",
+    "LinkedIn",
+    "YouTube",
+    "Facebook",
+    "Twitch",
+    "Dev.to",
+    "Codewars",
+    "Codepen",
+    "freeCodeCamp",
+    "GitLab",
+    "Hashnode",
+    "Stack Overflow"
+  ]
+
+  const save = () => {
+    console.log(listLinks)
+  }
+
+  const selectPlatform = (elt , index) => {
+    const newList = listLinks.map(element => {
+      if(element.id != index)
+        return {...element}
+      else 
+        return {...element , platform : elt}
+    })
+    setListlink(newList)
+    setOpenLinks({})
+  }
+  const setLink = (e , index) => {
+    const newList = listLinks.map(element => {
+      if(element.id != index)
+        return {...element}
+      else 
+        return {...element , link : e.target.value}
+    })
+    setListlink(newList)
+  }
 
    const  removeLink = (id) => {
     let newList = listLinks.filter(elt => elt.id !== id)
@@ -12,6 +57,15 @@ const Links = ({listLinks , setListlink}) => {
     const added = {id : listLinks.length + 1 , platform : "" , link : ""}
     setListlink([...listLinks  ,added])
   }
+  const toggleDropdown = (id) => {
+    setOpenLinks((prevOpenLinks) => ({
+      ...prevOpenLinks,
+      [id]: !prevOpenLinks[id],
+    }));
+  };
+
+
+
   return (
     <div className="col-span-5 lg:col-span-3 bg-white min-h-screen rounded-lg py-14 px-8">
         <h1 className='text-3xl font-bold'>Customize your links</h1>
@@ -19,16 +73,39 @@ const Links = ({listLinks , setListlink}) => {
         <button className='w-full border border-solid border-[#633cff] rounded-md py-2 flex justify-center items-center gap-3 font-bold text-[#633cff] hover:bg-[#633cff] hover:bg-opacity-10  mt-9' onClick={() => addLink()}> <IoMdAdd /> Add new link</button>
         {listLinks.map((elt , index) => 
                 <div className='w-full rounded-lg bg-[#fafafa] py-5 px-4 my-6 '>
-                    <div className='flex justify-between items-center '>
+                    <div className='flex justify-between items-center mb-4'>
                         <h2 className='text-lg text-[#737373] font-bold capitalize flex justify-center items-center'> <LuEqual /> Link #{index + 1}</h2>
                         <button className='hover:text-[#633cff] text-[#737373]' onClick={() => removeLink(elt.id)}>Remove</button>
+                    </div>
+                    <div className='mb-4 relative'>
+                      <p className='text-[#737373] text-sm mb-2'>Platform</p>
+                      <div className='relative' >
+                        <input type='text' className="input cursor-pointer" value={elt.platform} placeholder='Select platform' readOnly onClick={() => toggleDropdown(elt.id)} />
+                        <FaChevronDown className={`${openLinks[elt.id] && "rotate-180"} absolute right-4 top-[50%] translate-y-[-50%] text-[#633cff] transition-all duration-300 ease-in-out `} />
+                      </div>
+                      {openLinks[elt.id] && <div className=' z-10 absolute mt-3 max-h-60 w-full flex flex-col bg-white rounded-lg px-4 overflow-y-scroll shadow' >
+                        {
+                          platforms.map((elt1 , index) =>{ 
+                            const platformIcon = elt1.toLowerCase().replace(' ', '').replace('.', '')
+                            return( 
+                            <button className='w-full border-b border-solid text-[#333] hover:text-[#633cff] text-left py-4 flex justify-start items-center gap-3' onClick={() => selectPlatform(elt1 , elt.id)}>
+                                <img src={`/icons/select-icons/icon-${platformIcon}.svg`}  />
+                                {elt1}
+                              </button>)
+                          })
+                        }
+                        </div>}
+                    </div>
+                    <div className=''>
+                      <p className='text-[#737373] text-sm mb-2'>Link</p>
+                      <input type='text' className="input" placeholder='e.g.email@exemple.com' value={elt.link} onChange={(event) => setLink(event, elt.id)}/>
                     </div>
                 </div>
             )}
             
         <hr/>
         <div className='flex justify-end'>
-            <button className={`bg-[#633cff] text-white text-md rounded-lg py-2 px-6 font-semibold mt-4`}>
+            <button className={`bg-[#633cff] text-white text-md rounded-lg py-2 px-6 font-semibold mt-4`} onClick={save}>
                 Save
             </button>
         </div>
--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,28 +1,62 @@
-import React from 'react'
+import React , {useRef , useState} from 'react'
 import { IoMdAdd } from "react-icons/io";
 import { GrGallery } from "react-icons/gr";
+
 const Profile = () => {
+    const inputRef = useRef(null)
+  const [imageURL, setImageURL] = useState(null)
+const [imageSelected , setImageSelected] = useState(null)
+const [firstName , setFristName] = useState('')
+const [lastName , setLastName] = useState('')
+const [email , setEmail] = useState('')
+    const selectImage = () => {
+        inputRef.current.click()
+      }
+    
+    const handleImageChange = (e) => {
+        const selectedImage = e.target.files[0]
+        setImageSelected(selectedImage)
+        const imagePreview = URL.createObjectURL(selectedImage)
+        setImageURL(imagePreview)
+      }
   return (
     <div className="col-span-5 lg:col-span-3 bg-white min-h-screen rounded-lg py-14 px-8">
         <h1 className='text-3xl font-bold'>Profile Details</h1>
         <p className='text-[#737373] mt-4'>Add your details to create a personal touch to your profile.</p>
         <div className='w-full h-auto sm:h-[200px] rounded-lg bg-[#fafafa] py-5 px-4 my-6 flex items-start justify-between sm:items-center flex-col sm:flex-row gap-2 '>
-            <p className='w-full sm:w-[30%] text-[#737373]'>Profile picture</p>
-            <div className='w-[90%] sm:w-[300px] py-5 h-full text-[#633cff] bg-[#633cff] bg-opacity-10 flex justify-center items-center flex-col gap-6 rounded-lg'>
-            <GrGallery className='text-2xl font-bold'/>
-                <button className='flex justify-center items-center gap-3 font-semibold'> <IoMdAdd className='font-bold' /> Upload Image</button>
+            <p className='w-full sm:w-[30%] text-[#737373] '>Profile picture</p>
+            <div className='overflow-hidden relative w-[90%] sm:w-[300px] py-5 h-full text-[#633cff] bg-[#633cff] bg-opacity-10 flex justify-center items-center flex-col gap-6 rounded-lg'>
+            <GrGallery className='z-[10] text-2xl font-bold'/>
+                <button className='z-[10] flex justify-center items-center gap-3 font-semibold' onClick={selectImage}> <IoMdAdd className='font-bold' /> Upload Image</button>
+                {imageURL && (
+                    <img
+                    src={imageURL}
+                    alt="Selected"
+                    className="absolute w-full h-full "
+                    />
+                )}
             </div>
-            <p className='text-[#737373] text-sm'>Image must be below 1024x1024px. Use PNG or JPG format.</p>
+            <input
+                type="file"
+                accept="image/*"
+                ref={inputRef}
+                onChange={handleImageChange}
+                className="hidden"
+            />
+            <p className='text-[#737373] text-xs '>Image must be below 1024x1024px. Use PNG or JPG format.</p>
         </div>
         <div className='w-full  rounded-lg bg-[#fafafa] py-5 px-4 mb-6 '>
-            <div className='mb-4'>
-                <p className='text-[#737373]'>First name*</p>
+            <div className='mb-4 flex justify-between items-center gap-5 '>
+                <p className='text-[#737373] '>First name*</p>
+                <input type='text' className="!w-[75%] input" placeholder='e.g.Jhon' value={firstName} onChange={(e) => setFristName(e.target.value)} />
             </div>
-            <div className='mb-4'>
-                <p className='text-[#737373]'>Last name*</p>
+            <div className='mb-4 flex justify-between items-center gap-5'>
+                <p className='text-[#737373] '>Last name*</p>
+                <input type='text' className="!w-[75%] input" placeholder='e.g.Appleseed' value={lastName} onChange={(e) => setLastName(e.target.value)}  />
             </div>
-            <div>
-                <p className='text-[#737373]'>Email</p>
+            <div className='flex justify-between items-center gap-5'>
+                <p className='text-[#737373] '>Email</p>
+                <input type='text' className="!w-[75%] input" placeholder='e.g.email@exemple.com' value={email} onChange={(e) => setEmail(e.target.value)}  />
             </div>
         </div>
         <hr/>
